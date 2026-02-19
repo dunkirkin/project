3b19379bd9875c277bcf5fa290f6ab2ae4151dec
Jake Bowen <jakeeb05@gmail.com>
2026-02-18 19:56:06 -0800

    Added a logoutbutton that brings you back to login page

--- config/settings.py
+++ config/settings.py
@@ -120,5 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/logs/"
 LOGOUT_REDIRECT_URL = "/"
